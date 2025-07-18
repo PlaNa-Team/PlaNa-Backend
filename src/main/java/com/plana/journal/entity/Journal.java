faf5eaa0793e730f966b69b9331e2bdc296e45cb
daineee424 <daineee424@gmail.com>
2025-07-18 18:54:34 +0900

    feat: Project와 Journal 엔티티 생성 및 연관관계 매핑 추가

---
+++ src/main/java/com/plana/journal/entity/Journal.java
@@ -0,0 +1,68 @@
+package com.plana.journal.entity;
+
+import com.plana.auth.entity.Member;
+import com.plana.project.entity.Project;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "journal")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Journal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 연관 프로젝트
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    // 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "is_important", nullable = false)
+    private Boolean isImportant = false;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(length = 500, nullable = false)
+    private String detail;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/plana/project/entity/Project.java
@@ -0,0 +1,77 @@
+package com.plana.project.entity;
+
+import com.plana.auth.entity.Member;
+import com.plana.journal.entity.Journal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.List;
+
+@Entity
+@Table(name = "project")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // 해당 저널들
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Journal> journals;
+
+    @Column(nullable = false)
+    private Year year;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(name = "start_month", nullable = false)
+    private Integer startMonth;
+
+    @Column(name = "end_month", nullable = false)
+    private Integer endMonth;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    // 상태 값 ENUM
+    public enum Status {
+        예정, 진행, 완료, 중단
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
